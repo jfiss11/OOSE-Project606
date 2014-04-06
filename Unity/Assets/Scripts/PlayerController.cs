2feb49621db1ce5ec2a69fcc1a6576d23bb11f80
ceciliewarming <cwarming88@gmail.com>
2014-04-06 15:26:34 +0200

    Player controls
    Player controls; jump on space bar and left/right with arrows. Simple
    graphics added and project is now set to be a 2d project.

---
+++ Unity/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(PlayerPhysics))]
+
+public class PlayerController : MonoBehaviour {
+
+	//Player handeling
+	public float gravity = 20;
+	public float speed = 8;
+	public float acceleration = 30; 
+	public float jumpHeight = 12;
+
+	private float currentSpeed;
+	private float targetSpeed;
+	private Vector2 amountToMove;
+
+	private PlayerPhysics playerPhysics;
+
+	// Use this for initialization
+	void Start () {
+	
+		playerPhysics = GetComponent<PlayerPhysics>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		targetSpeed = Input.GetAxisRaw("Horizontal") * speed;
+		currentSpeed = IncrementTowards(currentSpeed, targetSpeed, acceleration);
+
+		if(playerPhysics.grounded){
+			//if player is on the ground, then jump. Jump is automatically set to the space bar.
+			amountToMove.y = 0;
+			if(Input.GetButtonDown("Jump")){
+				amountToMove.y = jumpHeight;
+
+			}
+		}
+
+		amountToMove.x = currentSpeed;
+		amountToMove.y -= gravity * Time.deltaTime;
+		playerPhysics.Move(amountToMove * Time.deltaTime);
+
+	}
+
+	//Increase n towards target by speed
+	private float IncrementTowards(float n, float target, float a){
+		if (n == target){
+			return n;
+		}
+		else{
+			float dir = Mathf.Sign(target - n); // Must n be increased or decreased to get closer to target
+			n += a * Time.deltaTime * dir;
+
+			// if n has now passed target then return target, orherwise return n
+			if(dir == Mathf.Sign(target-n)){
+				return n;
+			}
+			else{
+				return target;
+			}
+		}
+	}
+}
---
+++ Unity/Assets/Scripts/PlayerPhysics.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(BoxCollider))]
+
+// Will handle collisions 
+public class PlayerPhysics : MonoBehaviour {
+
+	public LayerMask collisionMask;
+
+	private BoxCollider collider;
+	private Vector3 s; // s = size
+	private Vector3 c; // c = centre
+
+	// to create a tiny bit of space between player and collider, to avoid the ray going through the ground
+	private float skin = .005f;
+
+	[HideInInspector]
+	public bool grounded;
+
+	Ray ray;
+	RaycastHit hit;
+
+	void Start(){
+		collider = GetComponent<BoxCollider>();
+		s = collider.size;
+		c = collider.center;
+	}
+
+
+	public void Move(Vector2 moveAmount){
+		float deltaY = moveAmount.y;
+		float deltaX = moveAmount.x;
+		Vector2 p = transform.position; //p = player
+
+		//assume player is not on the ground
+		grounded = false;
+
+		//Raycasting downwards to see if there is any ground below player, so p stops
+		for (int i = 0; i<3; i++){
+			float dir = Mathf.Sign(deltaY);
+			float x = (p.x + c.x - s.x/2) + s.x/2 * i; // Left, centre and then rightmost point of collider
+			float y = p.y + c.y + s.y/2 * dir; //buttom of collider
+
+			ray = new Ray(new Vector2(x,y), new Vector2(0, dir));
+			Debug.DrawRay(ray.origin, ray.direction);
+
+			// info about which other collider the ray hit
+			if(Physics.Raycast(ray, out hit, Mathf.Abs(deltaY), collisionMask)){
+				// Get distance between player and ground
+				float dst = Vector3.Distance(ray.origin, hit.point);
+
+				// stop player's downwards movement after coming within skin width of a collider
+				if (dst > skin){
+					deltaY = dst * dir + skin;
+				}
+				else{
+					deltaY = 0;
+				}
+				grounded = true;
+				break;
+			}
+		}
+
+			Vector2 finalTransform = new Vector2(deltaX, deltaY);
+		transform.Translate(finalTransform);
+	}
+
+}
